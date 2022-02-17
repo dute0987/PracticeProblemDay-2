0713e32e8d105929152c352166b833c5f759e618
dute0987 <dute0987@gmail.com>
2022-02-17 22:39:41 +0530

    shareing the day2 practice problem

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace LeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int year;
+            Console.WriteLine("Enter your year = ");
+            year = int.Parse(Console.ReadLine());
+
+            if (((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0)))
+            {
+                Console.WriteLine("This is leap year");
+            }
+            else
+            {
+                Console.WriteLine("this is not leap year");
+            }
+
+        }
+    }
+}
