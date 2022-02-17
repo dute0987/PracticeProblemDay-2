b14d04211941b1d530e6e4a66c2b6f9ac4a0987d
dute0987 <dute0987@gmail.com>
2022-02-18 00:50:27 +0530

    adding day-3 Factorial program

---
+++ FProgram.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace FactorialProgram
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            FactorialProgram.ProgramFactorail();
+        }
+    }
+}
