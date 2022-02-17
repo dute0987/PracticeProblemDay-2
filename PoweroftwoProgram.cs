282f2bbf9e45642df2266a7eb4da75940804376a
dute0987 <dute0987@gmail.com>
2022-02-18 00:09:56 +0530

    adding day-3 power of two program

---
+++ PoweroftwoProgram.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace PowerOfTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 4;
+            int y = 2;
+            int x = 1;
+            for (int i = 0; i < n; i++)
+            {
+                x = x * y;
+            }
+            Console.WriteLine(x);
+        }   
+    }
+}
