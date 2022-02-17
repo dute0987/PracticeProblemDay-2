0f9ed267e18f122ee9230a13c88d43e2f7775311
dute0987 <dute0987@gmail.com>
2022-02-18 00:43:16 +0530

    adding day-3 Harmonic program

---
+++ Harmonic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace HarmonicNumber
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("enter your number :");
+            int n = Convert.ToInt32(Console.ReadLine());
+            double x = 0.0;
+
+            for (double i = 1; i < n; i++)
+            {
+                x = x + 1/i;
+          
+            }
+            Console.WriteLine( x + " is the harmonic value ");
+        }
+    }
+}
