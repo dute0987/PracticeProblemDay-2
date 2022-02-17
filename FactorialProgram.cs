36eb24a2a41855444c0ac8f357d40af56d3b62e7
dute0987 <dute0987@gmail.com>
2022-02-18 00:51:38 +0530

    adding day-3 Factorial program file

---
+++ FactorialProgram.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialProgram
+{
+    public class FactorialProgram
+    {
+        public static void ProgramFactorail()
+        {
+            int n = 6;
+            double fact = 1;
+
+            for (int i=1; i<= n; i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("factorial of {0}={1}", n, fact);
+            Console.ReadLine();
+        }
+    }
+}
